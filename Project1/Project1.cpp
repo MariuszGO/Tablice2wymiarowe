f7dc4e79f888cdf73672efa5fc12236eafd68582
mariusz <mariusz@ADMIN>
2021-09-09 10:27:31 +0200

    Dodaj pliki projektów.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+
+int main() {
+	
+	srand (time(NULL));
+	
+	int tab[100][100];
+
+	for (int i = 0; i < 100; i++) {
+
+		for (int j = 0; j < 100; j++) {
+			tab[i][j] = rand() % 10 + 10;
+			//cout << &tab[i][j] << " ";
+
+		}
+
+	//	cout << endl;
+	}
+
+	cout << "koniec";
+}
